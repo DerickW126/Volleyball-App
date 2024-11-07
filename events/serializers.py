310f33940f7f0d110eadbe0563f9b206d134233d
Ubuntu <ubuntu@ip-172-31-35-113.ap-northeast-1.compute.internal>
2024-11-07 03:52:21 +0000

    Change permission settings

--- events/serializers.py
+++ events/serializers.py
@@ -63,7 +63,7 @@ class EventSerializer(serializers.ModelSerializer):
     
     def get_created_by_nickname(self, obj):
         return obj.created_by.nickname if hasattr(obj.created_by, 'nickname') else None
-    '''
+    
     def _serialize_registrations(self, registrations, viewer):
         serialized_data = []
         for registration in registrations:
@@ -77,7 +77,7 @@ class EventSerializer(serializers.ModelSerializer):
                 registration_data = RegistrationSerializer(registration).data
             serialized_data.append(registration_data)
         return serialized_data
-    '''
+    
 class ChatMessageSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
     user_id = serializers.IntegerField(source='user.id', read_only=True)
